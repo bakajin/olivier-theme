84ed0c5ac05a085800b7bde99cbba852a7c0972e
Olivier Oskamp <baka124@hotmail.com>
2014-03-11 16:55:19 +0100

    ui images and js for slideshow

---
+++ portal-theme/src/main/webapp/js/oss-deltares-slides.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+      		$('#slides-opensource').slidesjs({
+        		width: 300,
+       			height: 40,
+        		play: {
+          			active: false,
+          			auto: true,
+          			interval: 5000,
+          			swap: true
+        		}
+      		});
+      		$('#slides-opensoftware').slidesjs({
+        		width: 300,
+        		height: 40,
+        		play: {
+ 			        active: false,
+          			auto: true,
+          			interval: 6000,
+          			swap: true
+        		}
+      		});
+    	});
